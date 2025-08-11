ab188cd74b9b19e9a59b38c4fbfa67c6261c6184
LowSkillDeveloper <danil90381@mail.ru>
2025-08-11 20:06:55 +0200

    fix ico

--- app/src/main/java/com/lsd/wififrankenstein/ui/welcome/WelcomeThemeFragment.kt
+++ app/src/main/java/com/lsd/wififrankenstein/ui/welcome/WelcomeThemeFragment.kt
@@ -11,7 +11,6 @@ import androidx.fragment.app.activityViewModels
 import com.lsd.wififrankenstein.R
 import com.lsd.wififrankenstein.WelcomeViewModel
 import com.lsd.wififrankenstein.databinding.FragmentWelcomeThemeBinding
-import com.lsd.wififrankenstein.ui.settings.AppIconAdapter
 import com.lsd.wififrankenstein.ui.settings.SettingsViewModel
 
 class WelcomeThemeFragment : Fragment() {
@@ -20,7 +19,6 @@ class WelcomeThemeFragment : Fragment() {
     private val binding get() = _binding!!
     private val settingsViewModel: SettingsViewModel by activityViewModels()
     private val welcomeViewModel: WelcomeViewModel by activityViewModels()
-    private lateinit var iconAdapter: AppIconAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -66,34 +64,7 @@ class WelcomeThemeFragment : Fragment() {
     }
 
     private fun setupIconSettings() {
-        val icons = listOf(
-            Pair(getString(R.string.icon_default), R.mipmap.ic_launcher),
-            Pair(getString(R.string.icon_3wifi), R.mipmap.ic_launcher_3wifi),
-            Pair(getString(R.string.icon_anti3wifi), R.mipmap.ic_launcher_anti3wifi),
-            Pair(getString(R.string.icon_p3wifi), R.mipmap.ic_launcher_p3wifi),
-            Pair(getString(R.string.icon_p3wifi_pixel), R.mipmap.ic_launcher_p3wifi_pixel)
-        )
-
-        iconAdapter = AppIconAdapter(requireContext(), icons)
-        binding.spinnerAppIcon.adapter = iconAdapter
-
-        binding.spinnerAppIcon.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
-                val selectedIcon = when (position) {
-                    0 -> "default"
-                    1 -> "3wifi"
-                    2 -> "anti3wifi"
-                    3 -> "p3wifi"
-                    4 -> "p3wifi_pixel"
-                    else -> "default"
-                }
-                settingsViewModel.setAppIconDeferred(selectedIcon)
-                welcomeViewModel.setSelectedAppIcon(selectedIcon)
-            }
-
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-            }
-        }
+        binding.textViewIconDisabled.text = getString(R.string.icon_change_disabled_welcome)
     }
 
     override fun onDestroyView() {
