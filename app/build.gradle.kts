9964c419ed3ce5a4dd34ea0d3de3bd8bff38cb4a
LowSkillDeveloper <danil90381@mail.ru>
2026-08-25 11:57:33 +0200

    version 2.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "com.lsd.wififrankenstein"
         minSdk = 21
         targetSdk = 37
-        versionCode = 6
-        versionName = "2.3"
+        versionCode = 7
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
